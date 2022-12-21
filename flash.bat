75c7a63740923ef049aaf5124aa7d72b045eaf4c
William Forsdal <william.forsdal@gmail.com>
2022-12-21 16:54:58 +0100

    works, 364 bytes to blink LED

--- flash.bat
+++ flash.bat
@@ -1,4 +1,6 @@
 cargo rustc --release --target thumbv6m-none-eabi -- -C link-arg=-Tlink.x 
 if %errorlevel% neq 0 exit /b %errorlevel%
-arm-none-eabi-objcopy.exe -I elf32-littlearm -O srec ./target/thumbv6m-none-eabi/release/t4-rs ./target/thumbv6m-none-eabi/release/t4-rs.srec
+arm-none-eabi-objcopy -I elf32-littlearm -O srec ./target/thumbv6m-none-eabi/release/t4-rs ./target/thumbv6m-none-eabi/release/t4-rs.srec
+arm-none-eabi-objdump -x --wide --disassemble -l ./target/thumbv6m-none-eabi/release/t4-rs > ./target/thumbv6m-none-eabi/release/bin.srec.objdump
+arm-none-eabi-size.exe ./target/thumbv6m-none-eabi/release/t4-rs
 ST-LINK_CLI.exe -c UR -P "./target/thumbv6m-none-eabi/release/t4-rs.srec" -V -Rst
